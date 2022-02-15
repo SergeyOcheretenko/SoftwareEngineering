5523786c5ce96f3095215ffdd98054ce216949e6
SergeyOcheretenko <ocheretenko.s@gmail.com>
2022-02-15 23:57:43 +0200

    feat: add many-many-many bad code
    Add much examples of readline, readline-sync and Promises

--- Lab-work-1/src/index.js
+++ Lab-work-1/src/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// const readline = require('readline');
+const inquirer = require('inquirer');
+
 const squareSolver = (a, b, c) => {
 	const discriminant = b ** 2 - 4 * a * c;
 
@@ -12,20 +15,52 @@ const squareSolver = (a, b, c) => {
 	return [xOne, xTwo];
 };
 
-let readline = require('readline'); 
-let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: '> '
-});
+function consoleMethod() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-rl.prompt();
+(async () => {
+  const answer1 = await ask('Введите пример 1: ');
+  const answer2 = await ask('Введите пример 2: ');
+  console.log(`ответ1 = ${answer1}, ответ2 = ${answer2}`);
+})();
 
-rl.on('line', (input) => {
-    input = input.toLowerCase();
-    console.log(input);   
-    rl.close();
-});
+async function ask(question) {
+  return new Promise((resolve, reject) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question(question, (x) => {
+      rl.close();
+      resolve(x);
+    });
+  });
+}
+ 
+const readline = require('readline-sync');
+
+const name = readline.question("What is your name?");
+
+console.log("Hi " + name + ", nice to meet you.");
+
+const a = readline.question("What is your a?");
+
+console.log(a);
+
+const args = process.argv.slice(2);
+
+// let question = [
+//     {
+//         type: 'input',
+//         name: 'A'
+//     }
+// ];
+// inquirer.prompt(question).then(answer => {
+//     console.log(answer);
+// })
 
 // console.log(...squareSolver(1, 4, -5)); // 1 -5
 // console.log(...squareSolver(1, -4, 4)); // -2
