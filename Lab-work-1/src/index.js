01845c647676dda64ff26306589dd165e9558b79
SergeyOcheretenko <ocheretenko.s@gmail.com>
2022-02-16 20:17:34 +0200

    feat: add console readline a, b, c constants
    Import readline-sync module and add console readline input method

--- Lab-work-1/src/index.js
+++ Lab-work-1/src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const readline = require('readline-sync');
+
 const squareSolver = (a, b, c) => {
 	const discriminant = b ** 2 - 4 * a * c;
 
@@ -12,20 +14,11 @@ const squareSolver = (a, b, c) => {
 	return [xOne, xTwo];
 };
 
-let readline = require('readline'); 
-let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: '> '
-});
+const a = readline.question('Input number a: ');
+const b = readline.question('Input number b: ');
+const c = readline.question('Input number c: ');
 
-rl.prompt();
-
-rl.on('line', (input) => {
-    input = input.toLowerCase();
-    console.log(input);   
-    rl.close();
-});
+console.log(...squareSolver(a, b, c));
 
 // console.log(...squareSolver(1, 4, -5)); // 1 -5
 // console.log(...squareSolver(1, -4, 4)); // -2
