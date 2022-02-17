88f963aa5b3b19ffdf70be648cbf2ba6af72605b
SergeyOcheretenko <ocheretenko.s@gmail.com>
2022-02-17 16:30:34 +0200

    feat: move readline input in separate readline solving method function
    Move readline input in separate function. Change squareSolver array return to string return

--- Lab-work-1/src/index.js
+++ Lab-work-1/src/index.js
@@ -1,24 +1,32 @@
 'use strict';
 
 const readline = require('readline-sync');
+const args = process.argv.slice(2);
 
 const squareSolver = (a, b, c) => {
 	const discriminant = b ** 2 - 4 * a * c;
 
-	if (discriminant < 0) return ['Discriminant < 0'];
-	if (discriminant === 0) return [-b / (2 * a)];
+	if (discriminant < 0) 
+        return 'Discriminant < 0';
+	
+    if (discriminant === 0) 
+        return `Only one solution: ${-b / (2 * a)}`;
 
 	const xOne = (-b + discriminant ** 0.5) / (2 * a);
 	const xTwo = (-b - discriminant ** 0.5) / (2 * a);
 
-	return [xOne, xTwo];
+	return `First solution: ${xOne}, second solution: ${xTwo}`;
 };
 
-const a = readline.question('Input number a: ');
-const b = readline.question('Input number b: ');
-const c = readline.question('Input number c: ');
+const readlineMethod = () => {
+    const a = readline.question('Input number a: ');
+    const b = readline.question('Input number b: ');
+    const c = readline.question('Input number c: ');
+    
+    return squareSolver(a, b, c);
+};
 
-console.log(...squareSolver(a, b, c));
+console.log(readlineMethod());
 
 // console.log(...squareSolver(1, 4, -5)); // 1 -5
 // console.log(...squareSolver(1, -4, 4)); // -2
